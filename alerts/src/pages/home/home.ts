9ce9d98bb2dce5c77b98fa90bb49278c7e97886a
jajhonrod1620 <jajhonrod@gmail.com>
2018-06-05 13:21:31 -0500

    cambios alertas con opcion de integracion cordova npm

--- alerts/src/pages/home/home.ts
+++ alerts/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public alertCtrl: AlertController) {}
 
+    mostrarAlerta(){
+      let alerta = this.alertCtrl.create({
+        title: 'Este es el título',
+        subTitle: 'Este es el subtítulo',
+        message: 'Este es el mensaje',
+        buttons: ['OK']
+      });
+    }
   }
-
-}
