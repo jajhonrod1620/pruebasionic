e1cc3a234799f6ee31af5de0b6d74f9295196f9a
jajhonrod1620 <jajhonrod1@gmail.com>
2018-06-09 15:13:30 -0500

    menu lateral final 26

--- menus/src/app/app.component.ts
+++ menus/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { Platform, NavController } from 'ionic-angular';
+import { Platform, NavController, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -18,7 +18,7 @@ export class MyApp {
   amc = AmericacentralPage;
   amn = AmericanortePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public menuController: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -28,6 +28,7 @@ export class MyApp {
   }
   irAPagina(pagina:any){
     this.menu.setRoot(pagina);
+    this.menuController.close();
   }
 }
 
