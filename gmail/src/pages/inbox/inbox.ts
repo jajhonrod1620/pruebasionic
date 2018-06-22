7e10f3c3469853896fff4af44de744ce913f7918
jajhonrod1620 <jajhonrod1@gmail.com>
2018-06-21 21:09:00 -0500

    Floating action button correo nuevo 43

--- gmail/src/pages/inbox/inbox.ts
+++ gmail/src/pages/inbox/inbox.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { CorreosProvider } from '../../providers/correos/correos';
 import { CorreoPage } from './../correo/correo';
+import { NuevocorreoPage } from '../nuevocorreo/nuevocorreo';
 
 
 
@@ -17,13 +18,22 @@ export class InboxPage {
 
   constructor(public navCtrl: NavController, 
       public navParams: NavParams,
-      public correos:CorreosProvider) {
+      public correos:CorreosProvider,
+      public modalCtrl: ModalController) {
         this.listaCorreos = correos.lista;
   }
 
 
   verCorreo(correo){
     this.navCtrl.push(this.correoPage, correo);
+  }
+
+  nuevoCorreo(){
+    let modal = this.modalCtrl.
+      create(
+        NuevocorreoPage,
+         {tipo: 'nuevo correo',correo: ''});
+    modal.present();
   }
 
 
