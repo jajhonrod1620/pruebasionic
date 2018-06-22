7ed4885ae3a5339d4e72aa79ced4c0e948736998
jajhonrod1620 <jajhonrod1@gmail.com>
2018-06-21 21:40:41 -0500

    crear menulateral 44

--- gmail/src/app/app.component.ts
+++ gmail/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, NavController, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -11,14 +11,20 @@ import { InboxPage } from './../pages/inbox/inbox';
 })
 export class MyApp {
   rootPage:any = InboxPage;
+  @ViewChild('nav') menu: NavController;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public menuCtrl: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  irAPagina(pagina: any){
+    this.menu.setRoot(pagina);
+    this.menuCtrl.close();
   }
 }
 
