fad33c60937b0874ef894a71eae39a096c22f5a1
jajhonrod1620 <jajhonrod@gmail.com>
2018-06-26 13:04:49 -0500

    crear paginas aplicacion 52

--- restaurantes/src/app/app.module.ts
+++ restaurantes/src/app/app.module.ts
@@ -6,11 +6,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { RestaurantePage } from '../pages/restaurante/restaurante';
+import { AgregarRestaurantePage } from './../pages/agregar-restaurante/agregar-restaurante';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage, AgregarRestaurantePage, RestaurantePage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +22,7 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,  AgregarRestaurantePage, RestaurantePage
   ],
   providers: [
     StatusBar,
